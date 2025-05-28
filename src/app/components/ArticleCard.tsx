dd402008b5f9d3a3fb1e78fb9858365aac8d39c6
Mamonde02 <valerosojohnraymund@gmail.com>
2025-05-28 14:09:49 +0800

    initial article card component

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -5,8 +5,8 @@ export default function ArticleCard({ article }: { article: any }) {
                 <img src={article.urlToImage} alt={article.title} className="w-full h-48 object-cover" />
             )}
             <div className="p-4">
-                <h2 className="font-semibold text-lg">{article.title}</h2>
-                <p className="text-sm text-gray-600 my-2">{article.description}</p>
+                <h2 className="font-semibold text-gray-700 text-lg">{article.title}</h2>
+                <p className="mt-4 text-sm text-gray-600 my-2">{article.description}</p>
                 <a
                     href={article.url}
                     target="_blank"
